903d815a9c8a94fd64b2b1a110a6ec514ff150d9
toshytwx <dimon0272@gmail.com>
2018-05-23 00:38:19 +0300

    Add files via upload

---
+++ RSAEncryptor/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSAEncryptor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            FileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text Files | *.txt";
+            dialog.FilterIndex = 1;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textArea.Text = File.ReadAllText(dialog.FileName);
+            }
+        }
+
+        private void saveFileButton_Click(object sender, EventArgs e)
+        {
+            FileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Text Files | *.txt";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(dialog.FileName, textArea.Text);
+            }
+        }
+
+        private void aboutButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Was made by Antonkin D.O, TM-51, Variant 1");
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
